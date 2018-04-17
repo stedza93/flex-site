615141fc0c881dcc2f8d935ab9b95847fb6cfcd5
Stefan <stefan@Stefans-Mac-mini.local>
2018-04-17 14:56:21 +0200

    Site flex template

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("#magnify").click(function(){
+        $(this).hide();
+        $("#search").toggle("scale");
+        $("#search").css("display","block");
+    })
+
+});
+
